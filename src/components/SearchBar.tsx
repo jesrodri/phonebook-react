de12d0b2da9589d0959c09c4970632e57dcdbb36
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-10-20 17:45:54 -0300

    wip: search bar component

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+const SearchBar = () => {
+  return(
+    <form>
+      <input
+        type="text"
+        className="ml-20 mt-6 p-2 rounded-md"
+        placeholder="Search contacts..."
+      />
+    </form>
+  );
+};
+
+export default SearchBar;
