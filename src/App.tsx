1092c89dfa92049e7ed2c5adc28a710020b68b6d
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-10-20 17:47:40 -0300

    fix: update app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import ContactCard from './components/ContactCard';
-import SearchBar from './components/SearchBar';
 import './App.css';
 import contacts from "./assets/contacts.json";
 
 function App() {
   return (
     <div className="bg-gray-800">
-      <nav>
-        <SearchBar />
-      </nav>
       <section>
         <ContactCard contacts={contacts}/>
       </section>
