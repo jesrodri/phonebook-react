2de31a9ffeb6f49f031beaf2024c70666d3f8735
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-10-24 12:47:06 -0300

    chore: display all phone types

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -8,7 +8,7 @@ const ContactCard: Function = ({contacts}: Contacts) => {
   return(
     contacts.results.map((contact: any) => {
       return(
-        <div className="h-fit w-fit p-2 m-4 rounded bg-slate-400">
+        <div className="h-fit w-fit p-2 m-4 rounded">
           <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
             <img
               alt="contact"
@@ -19,9 +19,19 @@ const ContactCard: Function = ({contacts}: Contacts) => {
               <p className="text-gray-700 font-semibold text-xl mb-2">
                 {contact.name.first} {contact.name.last}
               </p>
-              <p className="text-gray-500">e-mail: {contact.email}</p>
-              <p>phone: {contact.phone}</p>
-              <p>cell: {contact.cell}</p>
+              <p className="text-gray-500">
+                <span className="font-medium">e-mail:</span> {contact.email}
+              </p>
+              {contact.numbers.map((number: any) => {
+                return(
+                  <p className="text-gray-500">
+                    <span className="font-medium">{number.type}:</span> {number.number}
+                  </p>
+                );
+              })}
+              <p className="text-gray-500">
+                <span className="font-medium">city:</span> {contact.location.city}
+              </p>
             </figcaption>
           </figure>
         </div>
